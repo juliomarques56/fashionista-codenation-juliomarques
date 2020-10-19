f102be5668107ba4f1d688dc47ca41fb30b3ca6d
Júlio Marques <jotamarques56@gmail.com>
2020-10-18 21:19:42 -0400

    Api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://undefined.netlify.app/api"
+    baseURL: "https://5f074b869c5c250016306cbf.mockapi.io/api/v1"
 })
 
 export default api;
